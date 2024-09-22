243dc0d0b9321baeaf91a5169fddfdd35f3c6c17
Ashiqur Rahman <32934763+Ashikak270@users.noreply.github.com>
2024-09-22 23:52:51 +0600

    Add files via upload

---
+++ assignment.dart
@@ -0,0 +1,29 @@
+class Book{
+    String title;
+    String author;
+    int  publicationYear;
+    int pagesRead;
+
+    read(int pages){
+        return pages;
+    }
+
+    getPagesRead(){
+        return pagesRead;
+    }
+
+    getTitle(){
+        return title;
+    }
+
+    getAuthor(){
+        return author;
+    }
+    getPublicationYear(){
+        return publicationYear;
+    }
+    getBookAge(){
+        int currentYear;
+        int subtract= currentYear - publicationYear;
+    }
+}
